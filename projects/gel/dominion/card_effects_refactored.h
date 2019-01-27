8c870062f502bb9d5a4714f69496d5412258f7fd
David Ge <david_ge@ultimatesoftware.com>
2019-01-26 22:57:39 -0800

    refactored 5 cards to have its own effect functions

---
+++ projects/gel/dominion/card_effects_refactored.h
@@ -0,0 +1,12 @@
+#ifndef GEL_CARD_EFFECTS_REFACTORED_H
+#define GEL_CARD_EFFECTS_REFACTORED_H
+
+#include "dominion.h"
+
+int adventurerEffect(struct gameState *state, int currentPlayer);
+int smithyEffect(struct gameState *state, int currentPlayer, int handPos);
+int villageEffect(struct gameState *state, int currentPlayer, int handPos);
+int council_roomEffect(struct gameState *state, int currentPlayer, int handPos);
+int remodelEffect(struct gameState *state, int currentPlayer, int handPos, int trashChoice, int gainChoice);
+
+#endif //GEL_CARD_EFFECTS_REFACTORED_H
