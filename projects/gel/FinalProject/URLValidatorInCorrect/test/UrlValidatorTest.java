774ff2d109bf190d806886fa7c59a6c401631c95
David Ge <david_ge@ultimatesoftware.com>
2019-03-13 23:49:14 -0700

    add unit tests

--- projects/gel/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/gel/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,49 +1,243 @@
+/*
+ * Winter 2019 - CS362 - Final Project Part B
+ * Matthew Solbrack, solbracm@oregonstate.edu
+ * Sung Hyun Pak, paks@oregonstate.edu
+ * Linge Ge, gel@oregonstate.edu
+ *
+ * Unit tests for the method isValid() in UrlValidator.
+ * Full statement and branch coverage.
+ */
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
-import junit.framework.TestCase;
-
-//You can use this as a skeleton for your 3 different test approach
-//It is an optional to use this file, you can generate your own test file(s) to test the target function!
-// Again, it is up to you to use this file or not!
-
-
-
-
-
-public class UrlValidatorTest extends TestCase {
-
-
-   public UrlValidatorTest(String testName) {
+public class UrlValidatorUnitTests extends TestCase {
+   public UrlValidatorUnitTests(String testName) {
       super(testName);
    }
 
-   
-   
-   public void testManualTest()
-   {
-//You can use this function to implement your manual testing	   
-	   
+   // mock these classes so that we mock certain methods in them in tests
+   Matcher mockUrlMatcher = mock(Matcher.class);
+   UrlValidator mockUrlValidator = mock(UrlValidator.class);
+   String mockString = mock(String.class);
+
+   public void testNullValuesAreRejected() {
+      // Arrange/Mock
+      UrlValidator validator = new UrlValidator();
+      String value = null;
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, false);
    }
-   
-   
-   public void testYourFirstPartition()
-   {
-	 //You can use this function to implement your First Partition testing	   
 
+   public void testUrlMatcherIsFalse() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(false);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, false);
    }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
 
+   public void testIsValidSchemeIsFalse() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(false);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, false);
    }
-   //You need to create more test cases for your Partitions if you need to 
-   
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
 
+   public void testIsValidAuthorityIsFalse() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(true);
+      when(mockString.equals(anyString())).thenReturn(false);
+      when(mockUrlValidator.isValidAuthority(anyString())).thenReturn(false);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, false);
    }
-   
 
+   public void testIsValidPathIsFalse() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(true);
+      when(mockString.equals(anyString())).thenReturn(false);
+      when(mockUrlValidator.isValidAuthority(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidPath(anyString())).thenReturn(false);
 
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, false);
+   }
+
+   public void testIsValidQueryIsFalse() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(true);
+      when(mockString.equals(anyString())).thenReturn(false);
+      when(mockUrlValidator.isValidAuthority(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidPath(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidQuery(anyString())).thenReturn(false);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, false);
+   }
+
+   public void testIsValidFragmentIsFalse() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(true);
+      when(mockString.equals(anyString())).thenReturn(false);
+      when(mockUrlValidator.isValidAuthority(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidPath(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidQuery(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidFragment(anyString())).thenReturn(false);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, false);
+   }
+
+   public void testPassAllValidations() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(true);
+      when(mockString.equals(anyString())).thenReturn(false);
+      when(mockUrlValidator.isValidAuthority(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidPath(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidQuery(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidFragment(anyString())).thenReturn(true);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, true);
+   }
+
+   public void testValidSpecialCaseFileWithColonAuthority() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_AUTHORITY)).thenReturn("not null");
+      when(mockString.equals(anyString())).thenReturn(true);
+      when(mockString.contains(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidAuthority(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidPath(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidQuery(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidFragment(anyString())).thenReturn(true);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, false);
+   }
+
+   public void testValidSpecialCaseFileWithNullAuthority() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_AUTHORITY)).thenReturn(null);
+      when(mockString.equals(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidAuthority(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidPath(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidQuery(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidFragment(anyString())).thenReturn(true);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, true);
+   }
+
+   public void testValidSpecialCaseFileWithoutColonAuthority() {
+      // Arrange/Mock
+      when(mockUrlMatcher.matches()).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_SCHEME)).thenReturn("file");
+      when(mockUrlValidator.isValidScheme(anyString())).thenReturn(true);
+      when(mockUrlMatcher.group(PARSE_URL_AUTHORITY)).thenReturn("not null");
+      when(mockString.equals(anyString())).thenReturn(true);
+      when(mockString.contains(anyString())).thenReturn(false);
+      when(mockUrlValidator.isValidAuthority(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidPath(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidQuery(anyString())).thenReturn(true);
+      when(mockUrlValidator.isValidFragment(anyString())).thenReturn(true);
+
+      UrlValidator validator = new UrlValidator();
+      String value = 'asdf';
+
+      // Act
+      boolean result = validator.isValid(value)
+
+      // Assert
+      assertEquals(result, true);
+   }
 }
